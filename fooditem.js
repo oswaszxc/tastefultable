3b57a1abcbb71a39364d5445b593cc8b845cd4ae
oswaszxc <152877854+oswaszxc@users.noreply.github.com>
2024-02-04 10:46:28 +0800

    Add files via upload

---
+++ fooditem.js
@@ -0,0 +1,237 @@
+const foodItem= [
+
+
+{
+    id: 3,
+    name: 'Pork Adobo',
+    category : 'biryani',
+    rating : 4.1,
+    price: 60,
+    img: 'images/1-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Bicol Express',
+    category : 'biryani',
+    rating : 4.3,
+    price: 60,
+    img: 'images/2-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Pork Steak',
+    category : 'biryani',
+    rating : 4.4,
+    price: 65,
+    img: 'images/3-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Pork Sinigang',
+    category : 'biryani',
+    rating : 3.9,
+    price: 60,
+    img: 'images/4-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Pork Giniling',
+    category : 'biryani',
+    rating : 4.9,
+    price: 60,
+    img: 'images/5-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Pork Menudo',
+    category : 'biryani',
+    rating : 4.0,
+    price: 55,
+    img: 'images/6-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Chicken Caldereta',
+    category : 'chicken',
+    rating : 4.3,
+    price: 50,
+    img: 'images/7-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Chicken Adobo',
+    category : 'chicken',
+    rating : 4.3,
+    price: 50,
+    img: 'images/8-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Chicken Tinola',
+    category : 'chicken',
+    rating : 4.7,
+    price: 50,
+    img: 'images/9-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Chicken Inasal',
+    category : 'chicken',
+    rating : 4.1,
+    price: 85,
+    img: 'images/10-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Fried Chicken w/Rice',
+    category : 'chicken',
+    rating : 4.3,
+    price: 60,
+    img: 'images/11-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Chicken Curry',
+    category : 'chicken',
+    rating : 4.7,
+    price: 55,
+    img: 'images/12-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Beef Bulalo',
+    category : 'paneer',
+    rating : 4.3,
+    price: 90,
+    img: 'images/13-removebg-preview.png',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Burger Steak w/Rice',
+    category : 'paneer',
+    rating : 4.9,
+    price: 60,
+    img: 'images/15-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Beef Pares',
+    category : 'paneer',
+    rating : 4.4,
+    price: 50,
+    img: 'images/16.png',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Beef Nilaga',
+    category : 'paneer',
+    rating : 4.0,
+    price: 50,
+    img: 'images/17-removebg-preview.png',
+    quantity: 1
+
+},
+
+{
+    id: 23,
+    name: 'Pritong Tilapia',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 50,
+    img: 'images/19-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Sarciadong Isda',
+    category : 'vegetable',
+    rating : 4.7,
+    price: 40,
+    img: 'images/20-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Paksiw na Bangus',
+    category : 'vegetable',
+    rating : 4.2,
+    price: 55,
+    img: 'images/21-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Sweet & Sour Tilapia',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 55,
+    img: 'images/22-removebg-preview.png',
+    quantity: 1
+},
+
+{
+    id: 30,
+    name: 'Pakbet',
+    category : 'chinese',
+    rating : 4.2,
+    price: 25,
+    img: 'images/23-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Langka',
+    category : 'chinese',
+    rating : 4.3,
+    price: 20,
+    img: 'images/24-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Monggo',
+    category : 'chinese',
+    rating : 4.8,
+    price: 25,
+    img: 'images/25-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Chopseuy',
+    category : 'chinese',
+    rating : 4.3,
+    price: 20,
+    img: 'images/26-removebg-preview.png',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Ginisang Upo',
+    category : 'chinese',
+    rating : 4.5,
+    price: 20,
+    img: 'images/27-removebg-preview.png',
+    quantity: 1
+},
+
+]
+
+export {foodItem};
